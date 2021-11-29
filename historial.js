5feaf47c78d989a450696b0b194316ebedfb4496
Martin Nogueroels <martin.nogueroles@gmail.com>
2021-11-29 17:59:04 -0300

    29/11 1758

--- historial.js
+++ historial.js
@@ -1,23 +1,4 @@
 
-// creamos un nuevo array que obtenga los datos del localStorage
-
-// let compras = [];
-
-// compras.push(JSON.parse(localStorage.getItem("selectoresHistorial")));
-
-
-// // Los datos del localStorage van a ordenarse de la siguiente manera en pantalla
-
-// compras[0].forEach(el => {
-//     let listaCompras = document.createElement("p");
-//     listaCompras.innerHTML = `has comprado ${el.cantidad} unidad/es de ${el.variedad}` 
-//     document.querySelector("#listaCompras").appendChild(listaCompras);
-// });
-
-//  vamos a traer mediante AJAX datos de usuarios para que se pueda ver quienes compraron en la tienda
-
-////    Declaramos una constante con el link a llamar
-
 const URLGET = "https://jsonplaceholder.typicode.com/users";
 
 //////  Llamamos a nuestro link mediante el boton de mostrar comentarios de usuarios
