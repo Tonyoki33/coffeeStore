1dec2646c2addd2e106399da7acdf23a3cce7a67
Martin Nogueroels <martin.nogueroles@gmail.com>
2021-12-28 13:57:23 -0300

    28/12 1357

--- historial.js
+++ historial.js
@@ -1,4 +1,17 @@
+//Traemos la información del local storage para ver el historial de compras
+let setHistorial = [];
+setHistorial = JSON.parse(localStorage.getItem("bolsasCafe"));
+setHistorial.forEach(el => {
+    $("#listaCompras").append(`
+        <div class="itmes_historial">
+            <h2 class="itmes_historial--title">${el.variedad}</h2>
+            <h3>${el.precio}$</h3>
+        </div>
+    `)
+});
 
+
+// creamos una variable con la url remota
 const URLGET = "https://jsonplaceholder.typicode.com/users";
 
 //////  Llamamos a nuestro link mediante el boton de mostrar comentarios de usuarios
@@ -11,7 +24,7 @@ $("#mostrarComentarios").on("click",function(e){
             for (const name of datos){
                 $("#comentariosUsuarios").append(
                     `
-                    <div class="user_comments">
+                    <div class="user_comments"> 
                     <h2>${name.name}</h2>
                     <p>Actualmente vivo en ${name.address.city} y mi pedido llegó existosamente, estoy supersatisfecho con el café</p>
                     </div>
--- main.js
+++ main.js
@@ -12,17 +12,14 @@ $(() => {
             this.selector = selector
         }
 
-        calcularStock(cantidad, position) {
+        calcularStock(cantidad) {
             let newStock = 0;
+            let placeToPrint = document.getElementById(`position-${this.id}`);
             if (cantidad <= this.stock) {
                 newStock = this.stock - cantidad;
-                let positiveNewStock = document.createElement("p");
-                positiveNewStock.setAttribute("id", "positiveStock");
-                document.getElementById(position).innerHTML = `Hay ${newStock} de bolsas disponibles`;
+                placeToPrint.innerHTML = `Hay ${newStock} bolsas disponibles`
             } else {
-                let negativeNewStock = document.createElement("p");
-                negativeNewStock.setAttribute("id", "negativeStock");
-                document.getElementById(position).innerHTML = `No hay stock de cafe ${this.variedad}`;
+                placeToPrint.innerHTML = `No hay stock de cafe ${this.variedad}`;
             }
             return this.stock = newStock;
         }
@@ -46,6 +43,7 @@ $(() => {
             }
         }
 
+
     }
 
 
@@ -53,14 +51,13 @@ $(() => {
     //definimos las variables
 
     let bolsasCafe = [];
+    let historial = [];
 
-    let carroCompras = [[]];
-
-    bolsasCafe.push(new Productos(0, "Sumatra", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHv3Tsc4BvdyNweyONNC7QI6XU9Z1GfuZLgA&usqp=CAU", 890, "oscuro", 14));
-    bolsasCafe.push(new Productos(1, "Colombia", "https://st3.depositphotos.com/3246763/14277/v/600/depositphotos_142778305-stock-illustration-coffee-logo-made-from-the.jpg", 890, "medio", 14));
-    bolsasCafe.push(new Productos(2, "Peru", "https://admin.tiendapower.com/storage/images/products/60b7aaad6dc781622649517.jpg", 890, "oscuro", 14));
-    bolsasCafe.push(new Productos(3, "Guatemala", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSROSog1kO-NqerHygFAmMSFAWM9zEgPlty1g&usqp=CAU", 910, "medio", 14));
-    bolsasCafe.push(new Productos(4, "Brasil", "https://d2r9epyceweg5n.cloudfront.net/stores/624/466/products/dsc018461-87578569c01cf9700215328738010466-1024-1024.png", 890, "rubio", 14));
+    bolsasCafe.push(new Productos(1, "Sumatra", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHv3Tsc4BvdyNweyONNC7QI6XU9Z1GfuZLgA&usqp=CAU", 890, "oscuro", 14));
+    bolsasCafe.push(new Productos(2, "Colombia", "https://st3.depositphotos.com/3246763/14277/v/600/depositphotos_142778305-stock-illustration-coffee-logo-made-from-the.jpg", 890, "medio", 14));
+    bolsasCafe.push(new Productos(3, "Peru", "https://admin.tiendapower.com/storage/images/products/60b7aaad6dc781622649517.jpg", 890, "oscuro", 14));
+    bolsasCafe.push(new Productos(4, "Guatemala", "https://admin.tiendapower.com/storage/images/products/60b6bf5f3b39a1622589279.jpg", 910, "medio", 14));
+    bolsasCafe.push(new Productos(5, "Brasil", "https://lh3.googleusercontent.com/VGlZRkYj1TIOuI0gVo_-TMqWPQxQqI-glqIFJO5vyaYkiekES6Nxlfa0_oe-kGVzBpGUeZWhnDkNWfPENqOZmq9lUPK6rrcX5Q", 890, "rubio", 14));
 
     //Vamos a mostrar la wallet con el dinero final
     let walletTitle = document.createElement("h3");
@@ -94,36 +91,28 @@ $(() => {
     const likeGuatemala = $("#like_btn--guatemala");
     const likeBrasil = $("#like_btn--brasil");
 
-    // representamos la lista del array let bolsasCafe = []; en el HTML
-    bolsasCafe.forEach(el => {
-        let bolsaTitulo = document.createElement("h2");
-        bolsaTitulo.innerHTML = el.variedad;
-        document.getElementById("product_data").appendChild(bolsaTitulo);
-
-        let cafeTostado = document.createElement("p");
-        cafeTostado.innerHTML = `El tostado es: ${el.tostado}`;
-        document.getElementById("product_data").appendChild(cafeTostado);
-
-        let cafeImg = document.createElement("img");
-        cafeImg.setAttribute("src", el.imagen);
-        cafeImg.setAttribute("class", "img_class");
-        document.getElementById("product_data").appendChild(cafeImg);
-
-        let cafePrecio = document.createElement("p");
-        cafePrecio.innerHTML = `${el.precio}`;
-        cafePrecio.setAttribute("class", "precio_p");
-        document.getElementById("product_data").appendChild(cafePrecio);
-
-        let cafeStock = document.createElement("p");
-        cafeStock.innerHTML = `Hay ${el.stock} de bolsas disponibles`;
-        cafeStock.setAttribute("id", `stock_atribute--${el.variedad}`);
-        document.getElementById("product_data").appendChild(cafeStock);
-
-    })
-
 
     //  definimos funciones
     ////    Enviaremos y codificaremos el array de precios
+
+    const fillGrid = (number) => {
+        $(`.product-${bolsasCafe[number].id}`).prepend(
+            ` 
+                <h2 class="card-title">${bolsasCafe[number].variedad}</h2>
+                <img src="${bolsasCafe[number].imagen}" alt="cafe-${bolsasCafe[number].variedad}"
+                class="card-img">
+                <p class="card-p">${bolsasCafe[number].tostado}</p>
+                <p id="position-${bolsasCafe[number].id}">Hay ${bolsasCafe[number].stock} bolsas disponiles</p>
+                <h3 class="card-price">${bolsasCafe[number].precio}</h3>
+                `);
+    }
+
+    fillGrid(0);
+    fillGrid(1);
+    fillGrid(2);
+    fillGrid(3);
+    fillGrid(4);
+
 
 
     const imprimirFondos = () => {
@@ -140,7 +129,7 @@ $(() => {
 
 
     const calcularResto = () => {
-        if (imprimirFondos() >= verificarPrecioTotal()){
+        if (imprimirFondos() >= verificarPrecioTotal()) {
             $("#chargedWallet").append(`
             <p>${imprimirFondos() - verificarPrecioTotal()}$</p>
             `);
@@ -153,26 +142,29 @@ $(() => {
     // calcular precio total
     const precioTotal = () => {
         let precioTotal = 0;
-        $(".eachPrice").each(function(){
+        $(".eachPrice").each(function () {
             let precioItem = parseFloat($(this).text());
-            precioTotal = precioTotal + precioItem; 
+            precioTotal = precioTotal + precioItem;
         })
         $("#total-price").text(`${precioTotal}$`);
         localStorage.setItem("totalCompra", JSON.stringify(precioTotal));
     }
 
-   
+
     const agregarItemCarrito = (a, b) => {
         //buscamos datos
         let nombre = a.variedad;
-        let cantidad = b.value; 
-        let precio = `<span class="eachPrice">${parseFloat(a.precio*cantidad)}</span>`;
+        let cantidad = b.value;
+        let precio = `<span class="eachPrice">${parseFloat(a.precio * cantidad)}</span>`;
         let remove = `<button class="remove">X</button>`
-        if (a.stock == 0){
+   
+        if (a.stock == 0) {
             $("#articulos").append(`<li>No hay mas articulos del seleccionado  ${remove}</li>`);
-        }else {
+        } else {
             $("#articulos").append(`<li>${cantidad} bolsas de cafe ${nombre} - $${precio}  ${remove}</li>`);
-        }    
+            historial.push(a);
+            localStorage.setItem("bolsasCafe", JSON.stringify(historial));
+        }
     }
 
 
@@ -181,9 +173,9 @@ $(() => {
     selectBtnSumatra.onclick = (e) => {
         e.preventDefault();
         bolsasCafe[0].calcularStock(selectSumtara.value, `stock_atribute--${bolsasCafe[0].variedad}`);
-        agregarItemCarrito(bolsasCafe[0],selectSumtara);
+        agregarItemCarrito(bolsasCafe[0], selectSumtara);
         precioTotal();
-        $(".remove").on("click",function(){
+        $(".remove").on("click", function () {
             $(this).parent().remove();
             precioTotal();
         })
@@ -193,9 +185,9 @@ $(() => {
     selectBtnColombia.onclick = (e) => {
         e.preventDefault();
         bolsasCafe[1].calcularStock(selectColombia.value, `stock_atribute--${bolsasCafe[1].variedad}`);
-        agregarItemCarrito(bolsasCafe[1],selectColombia);
+        agregarItemCarrito(bolsasCafe[1], selectColombia);
         precioTotal();
-        $(".remove").on("click",function(){
+        $(".remove").on("click", function () {
             $(this).parent().remove();
             precioTotal();
         })
@@ -205,21 +197,22 @@ $(() => {
     selectBtnPeru.onclick = (e) => {
         e.preventDefault();
         bolsasCafe[2].calcularStock(selectPeru.value, `stock_atribute--${bolsasCafe[2].variedad}`);
-        agregarItemCarrito(bolsasCafe[2],selectPeru);  
+        agregarItemCarrito(bolsasCafe[2], selectPeru);
         precioTotal();
-        $(".remove").on("click",function(){
+        $(".remove").on("click", function () {
             $(this).parent().remove();
             precioTotal();
         })
+        localStorage.setItem("bolsaCafe", JSON.stringify(bolsasCafe[2]));
     }
 
 
     selectBtnGuatemala.onclick = (e) => {
         e.preventDefault();
         bolsasCafe[3].calcularStock(selectGuatemala.value, `stock_atribute--${bolsasCafe[3].variedad}`);
-        agregarItemCarrito(bolsasCafe[3],selectGuatemala);
+        agregarItemCarrito(bolsasCafe[3], selectGuatemala);
         precioTotal();
-        $(".remove").on("click",function(){
+        $(".remove").on("click", function () {
             $(this).parent().remove();
             precioTotal();
         })
@@ -229,9 +222,9 @@ $(() => {
     selectBtnBrasil.onclick = (e) => {
         e.preventDefault();
         bolsasCafe[4].calcularStock(selectBrasil.value, `stock_atribute--${bolsasCafe[4].variedad}`);
-        agregarItemCarrito(bolsasCafe[4],selectBrasil);
+        agregarItemCarrito(bolsasCafe[4], selectBrasil);
         precioTotal();
-        $(".remove").on("click",function(){
+        $(".remove").on("click", function () {
             $(this).parent().remove();
             precioTotal();
         })
@@ -259,7 +252,7 @@ $(() => {
         });
     })
 
-    $("#compra_btn").on("click", (e) =>{
+    $("#compra_btn").on("click", (e) => {
         e.preventDefault();
         calcularResto(precioTotal());
         $(e.target).addClass("hide");
@@ -312,7 +305,7 @@ $(() => {
         $(".title").toggleClass("title_animation");
     })
 
-   
+
 })
 
 
